012606eefd2f2827cdd16ee0ce123b0a5a7c2919
Tuatha De Danann <114498537+DaniyalAtnagulov@users.noreply.github.com>
2025-06-19 14:39:28 +0300

    config.p_ rewrite_2

--- app/config.py
+++ app/config.py
@@ -1,19 +1,32 @@
-from typing import Literal, Optional
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal
 
 
-class BaseAppSettings(BaseSettings):
-    MODE: Literal["DEV", "TEST", "PROD"]
-    LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
-
+class DbSettings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
 
+    model_config = SettingsConfigDict(env_prefix="", env_file=".env")
+
+    @property
+    def url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+class TestDbSettings(DbSettings):
+    model_config = SettingsConfigDict(env_prefix="TEST_", env_file=".env")
+
+
+class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+    LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
+
     SMTP_HOST: str
-    SMTP_PORT: int 
+    SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASS: str
 
@@ -23,32 +36,10 @@ class BaseAppSettings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
+    db: DbSettings = DbSettings()
+    test_db: TestDbSettings = TestDbSettings()
+
     model_config = SettingsConfigDict(env_file=".env")
 
-    @property
-    def DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-
-class TestAppSettings(BaseAppSettings):
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_USER: str
-    TEST_DB_PASS: str
-    TEST_DB_NAME: str
-
-    @property
-    def DATABASE_TEST_URL(self):
-        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
-
-
-def get_settings():
-    import os
-    mode = os.getenv("MODE", "PROD")
-    if mode == "TEST":
-        return TestAppSettings()
-    else:
-        return BaseAppSettings()
-
-
-settings = get_settings()
+settings = Settings()
